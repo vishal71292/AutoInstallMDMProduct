ea42eec18925a9bbab3f519394529f6ca5ebf178
vishal71292 <vissas7@gmail.com>
2016-03-14 20:05:45 +0530

    Used Power Shell script to download and install Product

--- autoMDM.bat
+++ autoMDM.bat
@@ -1,8 +1,13 @@
-java -jar autoMDM.jar %1 %2 
-SET fileDownload=%2%
-if "%fileDownload%"=="" SET fileDownload="%cd%"
-7z x %fileDownload%/AdventNetMDM_WPack.zip -o%fileDownload%
-cd %fileDownload%/ManageEngine/DesktopCentral_Server/bin/scripts
+@echo off
+set downloadurl=%1
+set downloadpath=%2build.zip
+set directory=%2
+echo %downloadpath%
+echo %directory%
+%WINDIR%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& {Import-Module BitsTransfer;Start-BitsTransfer '%downloadurl%' '%downloadpath%';$shell = new-object -com shell.application;$zip = $shell.NameSpace('%downloadpath%');foreach($item in $zip.items()){$shell.Namespace('%directory%').copyhere($item);};remove-item '%downloadpath%';}"
+echo download complete and extracted to the directory.
+cd %directory%/ManageEngine/DesktopCentral_Server/bin/scripts
 CALL uninstall.bat
 CALL runOnce.bat
 CALL DCService.bat -t
+pause
